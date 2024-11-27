822a73daaf20a1b76a2caf2eb49a114af93fb72b
igorp <io006235@cfjl.com.br>
2024-11-27 14:36:37 -0300

    mudado o ip da api

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     env: {
-        //api: 'http://15.228.14.22',
-        api: 'http://localhost:8080',
+        api: 'http://15.228.14.22',
+        //api: 'http://localhost:8080',
     }, 
     reactStrictMode: true,
 };
